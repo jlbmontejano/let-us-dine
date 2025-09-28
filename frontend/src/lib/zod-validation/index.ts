751a458c35dc85733c99528ce288ca691ba5f93e
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-28 23:01:56 +1300

    Upgraded to zod v4, merged checkSession and joinSession schemas

--- frontend/src/lib/zod-validation/index.ts
+++ frontend/src/lib/zod-validation/index.ts
@@ -1,26 +1,32 @@
 import { z } from "zod";
-import { QUESTIONS } from "@/constants";
+import QUESTIONS from "../../../../shared/constants/questions";
 
 export const createSessionSchema = z.object({
 	totalParticipants: z.coerce
-		.number()
+		.number<number>("Insert a number between 2 and 12")
 		.gt(1, "Number must be between 2 and 12")
 		.lte(12, "Number must be between 2 and 12"),
 });
 
-export const joinSessionSchema = z.object({
-	sessionId: z.string().min(1, { message: "A session ID is required." }),
-});
-
 export const checkSessionSchema = z.object({
-	sessionId: z.string().min(1, { message: "A session ID is required." }),
+	sessionId: z
+		.uuidv4("Invalid ID")
+		.min(1, { message: "A session ID is required." }),
 });
 
 export const questionsSchema = z.object(
-	QUESTIONS.reduce((schema, question) => {
-		schema[question.text] = z.enum(question.options as [string, ...string[]], {
-			required_error: `An answer to each question is required.`,
-		});
-		return schema;
-	}, {} as Record<string, z.ZodTypeAny>)
+	QUESTIONS.reduce(
+		(schema, question) => {
+			const answerTexts = question.answers.map(answer => answer.text);
+
+			schema[question.text] = z.enum(
+				answerTexts as [string, ...string[]],
+				{
+					message: `An answer to "${question.text}" is required.`,
+				},
+			);
+			return schema;
+		},
+		{} as Record<string, z.ZodTypeAny>,
+	),
 );
