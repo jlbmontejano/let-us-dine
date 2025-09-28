a2d0100247f6d4ce28ee9c7f2f4660a627af7dd4
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-28 22:53:30 +1300

    Improved types

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,13 +1,53 @@
-export type PostSessionResultBody = {
-	questionnaireData: QuestionnaireData;
-	userLocation: GeolocationCoordinates;
+import { GooglePlace, TopAnswer } from "../../../shared/types";
+
+export type FindAnswerInfo = {
+	text: string;
+	question: {
+		id: number;
+		text: string;
+	};
 };
 
-export type QuestionnaireData = {
-	[key: string]: string;
+export type FindQuestionInfo = {
+	text: string;
+	answers: {
+		id: number;
+		text: string;
+	}[];
 };
 
-export type QuestionWithAnswer = {
-	question: string;
-	answer: string;
+export type FindResultInfo = {
+	sessionUuid: string;
+	latitude: number;
+	longitude: number;
+	answer: {
+		text: string;
+	};
+	question: {
+		text: string;
+	};
+};
+
+export type CreateSessionInfo = {
+	uuid: string;
+};
+
+export type FindSessionInfo = {
+	uuid: string;
+	isActive: boolean;
+	currentParticipants: number;
+	totalParticipants: number;
+};
+
+export type UserLocation = {
+	latitude: number;
+	longitude: number;
+	maxTravelDistance: number;
+};
+
+export type GooglePlacesParams = {
+	restaurantType: string;
+	latitude: number;
+	longitude: number;
+	radius: number;
 };
