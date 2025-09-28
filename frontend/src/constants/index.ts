fbbc980374e649dc51c318c0e19d0bfba80f4edf
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-28 23:03:03 +1300

    Moved question to shared folder

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,113 +1,34 @@
-export const QUESTIONS = [
-	{
-		id: "81",
-		key: "cuisine",
-		text: "What cuisine are you craving?",
-		options: ["Italian", "Japanese", "Mexican", "American", "Chinese", "Other"],
-	},
-	{
-		id: "83",
-		key: "budget",
-		text: "How much do you want to spend?",
-		options: ["Budget-friendly", "Mid-range", "Fancy dining"],
-	},
-	{
-		id: "82",
-		key: "travelDistance",
-		text: "How far are you willing to travel?",
-		options: [
-			"Within walking distance",
-			"10 minutes drive",
-			"20+ minutes drive",
-			"Any distance",
-		],
-	},
-	{
-		id: "90",
-		key: "diningPreference",
-		text: "Do you prefer dine-in, takeout, or delivery?",
-		options: ["Dine-in", "Takeout", "Delivery"],
-	},
-	{
-		id: "85",
-		key: "dietaryRestrictions",
-		text: "Do you have any dietary restrictions or preferences?",
-		options: [
-			"Vegetarian",
-			"Vegan",
-			"Gluten-free",
-			"Halal",
-			"No preferences",
-			"Other",
-		],
-	},
-	{
-		id: "88",
-		key: "mealType",
-		text: "Are you in the mood for a particular meal type?",
-		options: ["Breakfast", "Brunch", "Lunch", "Dinner", "Late-night snacks"],
-	},
-	{
-		id: "87",
-		key: "ambiance",
-		text: "How important is ambiance to you?",
-		options: ["Casual", "Family-friendly", "Romantic", "Lively", "Quiet"],
-	},
-	{
-		id: "85",
-		key: "beverages",
-		text: "Do you want alcohol or specialty beverages with your meal?",
-		options: ["Cocktails", "Wine", "Craft beer", "Mocktails", "No preference"],
-	},
-	{
-		id: "89",
-		key: "trendPreference",
-		text: "Are you looking for something trendy or a familiar favorite?",
-		options: [
-			"Trying new spots",
-			"Sticking to well-known places",
-			"No preference",
-		],
-	},
-	{
-		id: "84",
-		key: "waitTime",
-		text: "What’s the group’s preference for wait time?",
-		options: ["No wait required", "Okay with reservations", "Flexible"],
-	},
-];
-
 export const CAROUSEL_IMAGES = [
 	{
-		src: "fruit.jpg",
+		src: "images/fruit.jpg",
 		alt: "fruit",
 	},
 	{
-		src: "fusion.jpg",
+		src: "images/fusion.jpg",
 		alt: "fusion",
 	},
 	{
-		src: "hamburger.jpg",
+		src: "images/hamburger.jpg",
 		alt: "hamburger",
 	},
 	{
-		src: "japanese.jpg",
+		src: "images/japanese.jpg",
 		alt: "japanese",
 	},
 	{
-		src: "pancakes.jpg",
+		src: "images/pancakes.jpg",
 		alt: "pancakes",
 	},
 	{
-		src: "pasta.jpg",
+		src: "images/pasta.jpg",
 		alt: "pasta",
 	},
 	{
-		src: "tacos.jpg",
+		src: "images/tacos.jpg",
 		alt: "tacos",
 	},
 	{
-		src: "tiramisu.jpg",
+		src: "images/tiramisu.jpg",
 		alt: "tiramisu",
 	},
 ];
