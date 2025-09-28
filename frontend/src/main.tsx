9ba84363e655025635698beb080f15adf1ef1de3
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-28 22:56:44 +1300

    Added query provider and user location provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import App from "@/App.tsx";
 import "@/index.css";
-import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { UserLocationProvider } from "./context/user-location";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
+	<QueryClientProvider client={queryClient}>
 		<BrowserRouter>
-			<App />
+			<UserLocationProvider>
+				<App />
+			</UserLocationProvider>
 		</BrowserRouter>
-	</StrictMode>
+	</QueryClientProvider>,
 );
