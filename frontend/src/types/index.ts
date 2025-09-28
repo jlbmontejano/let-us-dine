0660e7f96ed191785d0725d7a760439ace0a2ee3
Jorge Laptop <jlbmontejano@gmail.com>
2025-09-28 22:59:47 +1300

    dELETED UNUSED TYPES

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,37 +1,15 @@
-export type SessionType = "join" | "create" | "check" | null;
+import { GooglePlace, TopAnswer } from "../../../shared/types";
 
-export type PostSession = {
-	uuid: string;
-};
-
-export type GetSession = {
-	uuid: string;
-	currentParticipants: number;
-	totalParticipants: number;
-	isActive: boolean;
-};
-
-export type SessionResults = {
-	averageLatitude: number | null;
-	averageLongitude: number | null;
-	questionsResults: QuestionResult[];
-};
-
-export type QuestionResult = {
-	text: string;
-	questionId: string;
-	questionText: string;
-	voteCount: number;
-};
+export type SessionType = "join" | "create" | "check" | "error" | null;
 
 export type SessionStatus = {
+	uuid: string;
 	isActive: boolean;
 	currentParticipants: number;
 	totalParticipants: number;
 };
 
-export type FetchResponse<T = undefined> = {
-	success: boolean;
-	data: T;
-	message?: string;
+export type SessionResult = {
+	places: GooglePlace[];
+	topAnswers: TopAnswer[];
 };
