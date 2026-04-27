cb603f8fceecd63fe101271d09cad7a56dff7455
Jorge Laptop <jlbmontejano@gmail.com>
2026-04-27 17:24:53 +1200

    Created SESSION_OPTIONS variable

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,4 +1,6 @@
-export const CAROUSEL_IMAGES = [
+import { SessionType } from "@/types";
+
+export const CAROUSEL_IMAGES: { src: string; alt: string }[] = [
 	{
 		src: "images/fruit.webp",
 		alt: "fruit",
@@ -31,4 +33,10 @@ export const CAROUSEL_IMAGES = [
 		src: "images/tiramisu.webp",
 		alt: "tiramisu",
 	},
+];
+
+export const SESSION_OPTIONS: { label: string; type: SessionType }[] = [
+	{ label: "Create a Session", type: "create" },
+	{ label: "Join a Session", type: "join" },
+	{ label: "Check Results", type: "check" },
 ];
