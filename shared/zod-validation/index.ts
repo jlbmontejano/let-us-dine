60b49f0794b25c47bd29e1058947851e3b4b8229
Jorge Laptop <jlbmontejano@gmail.com>
2026-05-07 09:20:15 +1200

    Deleted shared folder

--- shared/zod-validation/index.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const createSessionSchema = z.object({
-  totalParticipants: z
-    .number()
-    .gt(1, "Number must be between 2 and 12")
-    .lte(12, "Number must be between 2 and 12"),
-});
