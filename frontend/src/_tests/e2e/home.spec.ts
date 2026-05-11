72d72195cd85c2ed8f7b96511a3caa3416308710
Jorge Laptop <jlbmontejano@gmail.com>
2026-05-12 07:36:09 +1200

    Fixed typo

--- frontend/src/_tests/e2e/home.spec.ts
+++ frontend/src/_tests/e2e/home.spec.ts
@@ -2,9 +2,11 @@ import { test, expect } from "@playwright/test";
 
 test("user can navigate to setup and back home", async ({ page }) => {
 	await page.goto("/");
-	await page.getByRole("button", { name: "Get Started" }).click();
+	await page.getByRole("button", { name: "Get Started!" }).click();
 
-	expect(page.getByRole("heading", { name: "Session Setup" })).toBeVisible();
+	await expect(
+		page.getByRole("heading", { name: "Session Setup" }),
+	).toBeVisible();
 
 	await page.getByRole("button", { name: "Back" }).click();
 
